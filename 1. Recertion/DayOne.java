db85d7631b4b4ef1a35088e75dd630e867bdf05c
omzirpe2006 <omzirpe2006@gmail.com>
2025-11-16 10:20:50 +0530

    Question 2

--- 1. Recertion/DayOne.java
+++ 1. Recertion/DayOne.java
@@ -8,10 +8,15 @@ public class DayOne{
         q1(n-1);
         System.out.println(n);
     }
-
+    public static void q2(int n){
+        if(n<1)
+            return ;
+        System.out.println(n);
+        q2(n-1);
+    }
 
     public static void main(String[]args){
-        q1(5);
+        q2(5);
 
     }
     
